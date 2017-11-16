1c3d85fdf7d275b5367b1b5e11a8b38e2910dcb6
Rodrigo Amaral <rodrigoamaral@gmail.com>
2017-11-16 20:35:05 -0300

    Remove debug output

--- src/main/java/net/rodrigoamaral/dspsp/adapters/JMetalDSPSPAdapter.java
+++ src/main/java/net/rodrigoamaral/dspsp/adapters/JMetalDSPSPAdapter.java
@@ -42,9 +42,10 @@ public class JMetalDSPSPAdapter {
      */
     public JMetalDSPSPAdapter(String configFile) throws FileNotFoundException {
         this.project = new DynamicProjectConfigLoader(configFile).createProject();
-        for (int i = 0; i < 21; i++) {
-            System.out.println(i + "\t: " + project.isEmployeeAvailable(project.getEmployeeById(4), i));
-        }
+//        temp debug output
+//        for (int i = 0; i < 21; i++) {
+//            System.out.println(i + "\t: " + project.isEmployeeAvailable(project.getEmployeeById(4), i));
+//        }
         this.objectiveEvaluator = new SPSPObjectiveEvaluator()
                 .addObjective(new CostObjective())
                 .addObjective(new DurationObjective());
